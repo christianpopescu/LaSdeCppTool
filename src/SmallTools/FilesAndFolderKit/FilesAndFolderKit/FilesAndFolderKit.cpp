1b278900520e4f0d44ac4f4097464113eda264fe
Christian Popescu <christian.popescu@outlook.com>
2021-12-13 14:27:16 +0100

    add get message info input buffer

--- src/SmallTools/FilesAndFolderKit/FilesAndFolderKit/FilesAndFolderKit.cpp
+++ src/SmallTools/FilesAndFolderKit/FilesAndFolderKit/FilesAndFolderKit.cpp
@@ -2,11 +2,20 @@
 //
 
 #include <iostream>
+#include <string>
 #include <Windows.h>
+
+
+HANDLE hStdin;
+VOID ErrorExit(LPSTR, DWORD);
+VOID KeyEventProc(KEY_EVENT_RECORD);
+VOID MouseEventProc(MOUSE_EVENT_RECORD);
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD);
 
 bool test_ansi_code(int& value1)
 {
 	HANDLE hStdOut;
+	
 
 	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -20,7 +29,7 @@ bool test_ansi_code(int& value1)
 
 	// Hold original mode to restore on exit to be cooperative with other command-line apps.
 	const DWORD originalMode = mode;
-	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING | ENABLE_WINDOW_INPUT;
 
 	// Try to set the mode.
 	if (!SetConsoleMode(hStdOut, mode))
@@ -65,8 +74,8 @@ bool test_ansi_code(int& value1)
 	
 	std::string s;
 	std::cin >> s;
-	wprintf(L"\x1b[31mThis text has a red foreground using SGR.31.\r\n");
-	wprintf(L"\x1b[1mThis text has a bright (bold) red foreground using SGR.1 to affect the previous color setting.\r\n");
+	wprintf(L"\x1b[31mText 1.\r\n");
+	wprintf(L"\x1b[1mText 2.\r\n");
 	wprintf(L"\x1b[mThis text has returned to default colors using SGR.0 implicitly.\r\n");
 	wprintf(L"\x1b[34;46mThis text shows the foreground and background change at the same time.\r\n");
 	wprintf(L"\x1b[0mThis text has returned to default colors using SGR.0 explicitly.\r\n");
@@ -75,6 +84,65 @@ bool test_ansi_code(int& value1)
 	wprintf(L"\x1b[49mThis text has restored the background color only.\r\n");
 	// To also clear the scroll back, emit L"\x1b[3J" as well.
 	// 2J only clears the visible window and 3J only clears the scroll back.
+
+	DWORD cNumRead, fdwMode, i;
+	INPUT_RECORD irInBuf[128];
+	int counter = 0;
+
+	// Get the standard input handle.
+
+	hStdin = GetStdHandle(STD_INPUT_HANDLE);
+	std::string msg1{ "GetStdHandle"};
+	//LPSTR pS1 =  TCHAR[31];
+	LPSTR pS1 = const_cast<char*>(msg1.c_str());
+	std::string msg2{ "ReadConsoleInput" };
+	//LPSTR pS1 =  TCHAR[31];
+	LPSTR pS2 = const_cast<char*>(msg2.c_str());
+	std::string msg3{ "ReadConsoleInput" };
+	//LPSTR pS1 =  TCHAR[31];
+	LPSTR pS3 = const_cast<char*>(msg3.c_str());
+	if (hStdin == INVALID_HANDLE_VALUE)
+		ErrorExit(pS1, originalMode);
+	while (counter++ <= 100)
+	{
+		// Wait for the events.
+
+		if (!ReadConsoleInput(
+			hStdin,      // input buffer handle
+			irInBuf,     // buffer to read into
+			128,         // size of read buffer
+			&cNumRead)) // number of records read
+			ErrorExit(pS2, originalMode);
+
+		// Dispatch the events to the appropriate handler.
+
+		for (i = 0; i < cNumRead; i++)
+		{
+			switch (irInBuf[i].EventType)
+			{
+			case KEY_EVENT: // keyboard input
+				KeyEventProc(irInBuf[i].Event.KeyEvent);
+				break;
+
+			case MOUSE_EVENT: // mouse input
+				MouseEventProc(irInBuf[i].Event.MouseEvent);
+				break;
+
+			case WINDOW_BUFFER_SIZE_EVENT: // scrn buf. resizing
+				ResizeEventProc(irInBuf[i].Event.WindowBufferSizeEvent);
+				break;
+
+			case FOCUS_EVENT:  // disregard focus events
+
+			case MENU_EVENT:   // disregard menu events
+				break;
+
+			default:
+				ErrorExit(pS3, originalMode);
+				break;
+			}
+		}
+	}
 
 	// Restore the mode on the way out to be nice to other command-line applications.
 	SetConsoleMode(hStdOut, originalMode);
@@ -89,4 +157,70 @@ int main()
 	return 0;
 }
 
+VOID ErrorExit(LPSTR lpszMessage, DWORD savedMode)
+{
+	fprintf(stderr, "%s\n", lpszMessage);
 
+	// Restore input mode on exit.
+	
+	SetConsoleMode(hStdin, savedMode);
+
+	ExitProcess(0);
+}
+
+VOID KeyEventProc(KEY_EVENT_RECORD ker)
+{
+	printf("Key event: ");
+
+	if (ker.bKeyDown)
+		printf("key pressed\n");
+	else printf("key released\n");
+}
+
+VOID MouseEventProc(MOUSE_EVENT_RECORD mer)
+{
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+	printf("Mouse event: ");
+
+	switch (mer.dwEventFlags)
+	{
+	case 0:
+
+		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+		{
+			printf("left button press \n");
+		}
+		else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
+		{
+			printf("right button press \n");
+		}
+		else
+		{
+			printf("button press\n");
+		}
+		break;
+	case DOUBLE_CLICK:
+		printf("double click\n");
+		break;
+	case MOUSE_HWHEELED:
+		printf("horizontal mouse wheel\n");
+		break;
+	case MOUSE_MOVED:
+		printf("mouse moved\n");
+		break;
+	case MOUSE_WHEELED:
+		printf("vertical mouse wheel\n");
+		break;
+	default:
+		printf("unknown\n");
+		break;
+	}
+}
+
+VOID ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
+{
+	printf("Resize event\n");
+	printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
+}
