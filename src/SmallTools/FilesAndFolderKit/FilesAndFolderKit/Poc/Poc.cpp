7e6b9f0362dff3366787694e8e433baf822190da
Christian Popescu <christian.popescu@outlook.com>
2022-01-14 22:53:57 +0100

    Refactoring FilesAndFolderKit

--- src/SmallTools/FilesAndFolderKit/FilesAndFolderKit/Poc/Poc.cpp
+++ src/SmallTools/FilesAndFolderKit/FilesAndFolderKit/Poc/Poc.cpp
@@ -1,5 +1,86 @@
 #include "Poc.h"
 
+VOID Poc::ErrorExit(LPSTR lpszMessage, DWORD savedMode)
+{
+	fprintf(stderr, "%s\n", lpszMessage);
+
+	// Restore input mode on exit.
+
+	SetConsoleMode(hStdin, savedMode);
+
+	ExitProcess(0);
+
+}
+VOID Poc::ErrorExit(LPSTR lpszMessage, DWORD savedMode)
+{
+	fprintf(stderr, "%s\n", lpszMessage);
+
+	// Restore input mode on exit.
+
+	SetConsoleMode(hStdin, savedMode);
+
+	ExitProcess(0);
+}
+
+
+VOID Poc::MouseEventProc(MOUSE_EVENT_RECORD mer)
+{
+#ifndef MOUSE_HWHEELED
+#define MOUSE_HWHEELED 0x0008
+#endif
+	printf("Mouse event: ");
+
+	switch (mer.dwEventFlags)
+	{
+	case 0:
+
+		if (mer.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
+		{
+			printf("left button press \n");
+		}
+		else if (mer.dwButtonState == RIGHTMOST_BUTTON_PRESSED)
+		{
+			printf("right button press \n");
+		}
+		else
+		{
+			printf("button press\n");
+		}
+		break;
+	case DOUBLE_CLICK:
+		printf("double click\n");
+		break;
+	case MOUSE_HWHEELED:
+		printf("horizontal mouse wheel\n");
+		break;
+	case MOUSE_MOVED:
+		printf("mouse moved\n");
+		break;
+	case MOUSE_WHEELED:
+		printf("vertical mouse wheel\n");
+		break;
+	default:
+		printf("unknown\n");
+		break;
+	}
+}
+
+
+VOID Poc::KeyEventProc(KEY_EVENT_RECORD ker)
+{
+	printf("Key event: ");
+
+	if (ker.bKeyDown)
+		printf("key pressed\n");
+	else printf("key released\n");
+}
+
+VOID Poc::ResizeEventProc(WINDOW_BUFFER_SIZE_RECORD wbsr)
+{
+	printf("Resize event\n");
+	printf("Console screen buffer is %d columns by %d rows.\n", wbsr.dwSize.X, wbsr.dwSize.Y);
+}
+
 PWSTR Poc::LoadFile()
 {
 
