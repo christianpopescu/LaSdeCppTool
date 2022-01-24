e77b0783e15c6452f55d5ff0ef24041cb6c85ac0
Christian Popescu <christian.popescu@outlook.com>
2022-01-25 00:41:38 +0100

    Working example of iterating and filtering on regex

--- src/SmallTools/WorkstationManager/ConsoleConceptTest/ConsoleConceptTest.cpp
+++ src/SmallTools/WorkstationManager/ConsoleConceptTest/ConsoleConceptTest.cpp
@@ -2,9 +2,21 @@
 //
 
 #include <iostream>
+#include <filesystem>
+#include <regex>
+#include "../FileAndFoldersLib/FileAndFolderService.h"
+
+
+namespace fs = std::filesystem;
 
 int main()
 {
     std::cout << "Hello World!\n";
+    fs::path pt("D:\\Temp\\FirstQtWidgets");
+    for (auto p : FileAndFolderService::GetRecursiveFiles(pt, std::regex(".*"))) {
+        std::cout << p.stem().string() << std::endl;
+    }
+
+
 }
 
--- src/SmallTools/WorkstationManager/FileAndFoldersLib/FileAndFolderService.cpp
+++ src/SmallTools/WorkstationManager/FileAndFoldersLib/FileAndFolderService.cpp
@@ -1,14 +1,18 @@
 #include "pch.h"
 #include "FileAndFolderService.h"
 
-std::vector<fs::path> FileAndFolderService::GetRecursiveFiles(const fs::path&  inputPath, const std::regex&  reg)
+std::vector<fs::path> FileAndFolderService::GetRecursiveFiles(const fs::path& inputPath, const std::regex& reg)
 {
+
     std::vector<fs::path> result;
-    for (const fs::directory_entry& de :
-        fs::recursive_directory_iterator(inputPath)) {
-        if (de.is_regular_file() && std::regex_match(de.path().stem().string(), reg)) {
-            result.push_back(de.path());
+    if (fs::exists(inputPath)) {
+        for (const fs::directory_entry& de :
+            fs::recursive_directory_iterator(inputPath)) {
+            if (de.is_regular_file() && std::regex_match(de.path().stem().string(), reg)) {
+                result.push_back(de.path());
+            }
+
         }
-        return result;
     }
+    return result;
 }
