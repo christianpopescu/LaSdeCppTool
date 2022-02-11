98875809b5c91997b38a1ef29958557fb338c7c3
Christian Popescu <christian.popescu@outlook.com>
2022-02-11 17:22:55 +0100

    add search in file by regular expression

--- src/SmallTools/WorkstationManager/FileAndFoldersLib/FileAndFolderService.cpp
+++ src/SmallTools/WorkstationManager/FileAndFoldersLib/FileAndFolderService.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "FileAndFolderService.h"
+#include <fstream>
 
 std::vector<fs::path> FileAndFolderService::GetRecursiveFiles(const fs::path& inputPath, const std::regex& reg)
 {
@@ -12,6 +13,28 @@ std::vector<fs::path> FileAndFolderService::GetRecursiveFiles(const fs::path& in
                 result.push_back(de.path());
             }
 
+        }
+    }
+    return result;
+}
+
+std::vector<std::vector<std::string>> FileAndFolderService::GetInfoFromFile(const fs::path& filePath, const std::regex& reg, int number_of_groups)
+{
+    std::vector<std::vector<std::string>> result;
+    if (fs::exists(filePath) && fs::is_regular_file(filePath)) {
+        std::ifstream file;
+        file.open(filePath.string());
+        std::string line;
+        std::vector<std::string > line_by_group;
+        if (file.is_open()) {
+            std::smatch match;
+            if (std::regex_search(line, match, reg) == true) {
+                std::vector<std::string > line_by_group;
+                for (int i{ 0 }; i < number_of_groups; ++i) {
+                    line_by_group.push_back(match.str(i + 1));
+                }
+                result.push_back(line_by_group);
+            }
         }
     }
     return result;
--- src/SmallTools/WorkstationManager/FileAndFoldersLib/FileAndFolderService.h
+++ src/SmallTools/WorkstationManager/FileAndFoldersLib/FileAndFolderService.h
@@ -6,5 +6,9 @@ class FileAndFolderService
 {
 public:
 	static std::vector<fs::path> GetRecursiveFiles(const fs::path&  inputPath, const std::regex&  reg);
+	
+	// Return list of groups get from regular expression
+	// Assumption: the file contains lines that corresponds to regular expression. The regular expression could contains a couple of groups
+	static std::vector<std::vector<std::string>> GetInfoFromFile(const fs::path& filePath, const std::regex& reg, int number_of_groups);
 };
 
